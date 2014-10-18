e34fadd5625198aca9bf6b7efd37c9c0a3074651
David Tschida <dmtschida1@gmail.com>
2014-10-18 16:19:25 -0400

    Added chat functionality.

--- app/src/main/java/me/gethelloworld/android/youhadmeathelloworld/ChatActivity.java
+++ app/src/main/java/me/gethelloworld/android/youhadmeathelloworld/ChatActivity.java
@@ -1,36 +1,138 @@
 package me.gethelloworld.android.youhadmeathelloworld;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.SharedPreferences;
+import android.database.DataSetObserver;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.*;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
-public class ChatActivity extends Activity {
+import me.gethelloworld.android.youhadmeathelloworld.R;
+import me.gethelloworld.android.youhadmeathelloworld.adapters.ChatListAdapter;
+import me.gethelloworld.android.youhadmeathelloworld.auth.AuthenticationManager;
+import me.gethelloworld.android.youhadmeathelloworld.data.Chat;
+
+public class ChatActivity extends ListActivity {
+
+    // TODO: change this to your own Firebase URL
+    private static final String FIREBASE_URL = "https://yhmahw.firebaseIO.com";
+
+    private String username;
+    private Firebase ref;
+    private ValueEventListener connectedListener;
+    private ChatListAdapter chatListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        // Make sure we have a username
+        setupUsername();
+
+        setTitle("Chatting as " + username);
+
+        // Setup our Firebase ref
+        ref = new Firebase(FIREBASE_URL).child("chat").child(createChildIdFromUsers(getIntent().getStringExtra("match_user")));
+
+        // Setup our input methods. Enter key on the keyboard or pushing the send button
+        EditText inputText = (EditText)findViewById(R.id.messageInput);
+        inputText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_NULL && keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
+                    sendMessage();
+                }
+                return true;
+            }
+        });
+
+        findViewById(R.id.sendButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+        });
+
     }
 
+    private String createChildIdFromUsers(String match_user) {
+        ArrayList<String> arr = new ArrayList<String>(2);
+        arr.add(match_user);
+        arr.add( username );
+        Collections.sort(arr);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.chat, menu);
-        return true;
+
+        String chatID = arr.get(0) + "|" + arr.get(1);
+
+        return chatID; 
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    public void onStart() {
+        super.onStart();
+        // Setup our view and list adapter. Ensure it scrolls to the bottom as data changes
+        final ListView listView = getListView();
+        // Tell our list adapter that we only want 50 messages at a time
+        chatListAdapter = new ChatListAdapter(ref.limit(50), this, R.layout.chat_message, username);
+        listView.setAdapter(chatListAdapter);
+        chatListAdapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                super.onChanged();
+                listView.setSelection(chatListAdapter.getCount() - 1);
+            }
+        });
+
+        // Finally, a little indication of connection status
+        connectedListener = ref.getRoot().child(".info/connected").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                boolean connected = (Boolean)dataSnapshot.getValue();
+                if (connected) {
+                    Toast.makeText(ChatActivity.this, "Connected to Firebase", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(ChatActivity.this, "Disconnected from Firebase", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled() {
+                // No-op
+            }
+        });
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        ref.getRoot().child(".info/connected").removeEventListener(connectedListener);
+        chatListAdapter.cleanup();
+    }
+
+    private void setupUsername() {
+        username = AuthenticationManager.getUsername(this);
+    }
+
+    private void sendMessage() {
+        EditText inputText = (EditText)findViewById(R.id.messageInput);
+        String input = inputText.getText().toString();
+        if (!input.equals("")) {
+            // Create our 'model', a Chat object
+            Chat chat = new Chat(input, username);
+            // Create a new, auto-generated child of that chat location, and save our chat data there
+            ref.push().setValue(chat);
+            inputText.setText("");
         }
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/me/gethelloworld/android/youhadmeathelloworld/data/Chat.java
@@ -0,0 +1,30 @@
+package me.gethelloworld.android.youhadmeathelloworld.data;
+
+
+/**
+ * User: greg
+ * Date: 6/21/13
+ * Time: 2:38 PM
+ */
+public class Chat {
+
+    private String message;
+    private String author;
+
+    // Required default constructor for Firebase object mapping
+    @SuppressWarnings("unused")
+    private Chat() { }
+
+    public Chat(String message, String author) {
+        this.message = message;
+        this.author = author;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
